a96b42ee70278360159f00e4aa2c684fdf70ddaa
Vabka <vab-999@ya.ru>
2023-02-01 11:59:36 +0300

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 mod contracts;
-use contracts::tinkoff_public_invest_api_contract_v1::*;
+use std::{error::Error, fmt::Display};
+
 use contracts::tinkoff_public_invest_api_contract_v1::users_service_client::*;
-use tonic::{service::Interceptor, transport::Endpoint};
+use contracts::tinkoff_public_invest_api_contract_v1::*;
+use tonic::{
+    metadata::{Ascii, MetadataValue},
+    service::Interceptor,
+    transport::Endpoint,
+};
 
 #[derive(Clone)]
 struct TinkoffSpecificHeadersInterceptor {
-    token: String,
-    app_name: Option<String>,
+    authorization_header_value: MetadataValue<Ascii>,
+    x_app_name_header_value: MetadataValue<Ascii>,
+}
+impl TinkoffSpecificHeadersInterceptor {
+    fn new(token: impl Display) -> Result<Self, Box<dyn Error>> {
+        let authorization_header_value: MetadataValue<Ascii> =
+            format!("Bearer {token}").try_into()?;
+        let x_app_name_header_value = MetadataValue::<Ascii>::from_static("rust_sdk");
+        Ok(Self {
+            authorization_header_value,
+            x_app_name_header_value,
+        })
+    }
 }
 
 impl Interceptor for TinkoffSpecificHeadersInterceptor {
@@ -14,34 +31,20 @@ impl Interceptor for TinkoffSpecificHeadersInterceptor {
         &mut self,
         mut request: tonic::Request<()>,
     ) -> Result<tonic::Request<()>, tonic::Status> {
-        {
-            let metadata = request.metadata_mut();
-            let authorization_value = format!("Bearer {}", self.token)
-                .try_into()
-                .expect("token value");
-            metadata.insert("authorization", authorization_value);
-            if let Some(app_name) = &self.app_name {
-                let app_name = app_name.try_into().expect("app name");
-                metadata.insert("x-app-name", app_name);
-            }
-        }
+        let metadata = request.metadata_mut();
+        metadata.insert("authorization", self.authorization_header_value.clone());
+        metadata.insert("x-app-name", self.x_app_name_header_value.clone());
         Ok(request)
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let interceptor = TinkoffSpecificHeadersInterceptor {
-        token: std::env::var("TOKEN")?,
-        app_name: None,
-    };
-
     let uri = "https://invest-public-api.tinkoff.ru:443";
-
     let channel = Endpoint::new(uri)?.connect().await?;
-
-    let mut client =
-        UsersServiceClient::with_interceptor(channel, interceptor);
+    let interceptor = TinkoffSpecificHeadersInterceptor::new(std::env::var("TOKEN")?)?;
+    
+    let mut client = UsersServiceClient::with_interceptor(channel, interceptor);
     let response = client.get_accounts(GetAccountsRequest {}).await?;
     let message = response.into_inner();
     let accounts = &message.accounts;
